156cd8b8f10f7250b283e895d25234c35bfd0a55
Hazim ASRI <wan-muhammad-hazim.wan-mohd-asri@etu.univ-lyon1.fr>
2023-02-17 00:48:00 +0100

    Improved animation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-const roulette = document.getElementById('roulette');
+(()=> {
+    const roulette = document.getElementById('roulette');
+    const layover = document.getElementById('layover');
 
-roulette.addEventListener('click', () => {
-    roulette.classList.add('spin');
-    setTimeout(() => {
-        roulette.classList.remove('spin');
-    }, 10000);
-});
+    let degree = Math.floor(Math.random() * 360);
+
+    roulette.addEventListener('click', (e) => {
+
+        let force = Math.floor(Math.random() * 4) + 4;
+        let rotation = Math.floor(Math.random() * 360) + 360 * force;
+
+        let x = e.clientX;
+        let y = e.clientY;
+
+        console.log(`x: ${x}, y: ${y}`);
+
+        degree = degree + rotation;
+
+        roulette.style.transition = `all ${force}s ease-out`;
+        roulette.style.transform = `rotate(${degree}deg)`;
+
+        console.log(layover);
+
+        layover.style.left = `${x} px`;
+        layover.style.top = `${y} px`;
+    });
+
+    roulette.addEventListener('transitionend', () => {
+        roulette.style.transition = 'none';
+    });
+})();
